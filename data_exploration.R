2f2ac98bf4dc833a777ff4994c2df14c8171e174
윤정인 <yoonjeongin@naver.com>
2021-10-05 06:41:48 +0900

    operator

--- data_exploration.R
+++ data_exploration.R
@@ -50,3 +50,59 @@ exam_addition$id <- NULL #id 변수 지워주기
 
 ###데이터 프레임 내보내기
 write.csv(exam, file="exam1.csv")
+install.packages("writexl")
+library(writexl)
+write_xlsx(exam_addition, path = "exam2.xlsx")
+
+###비교 연산자 
+#exam에서 주소가 원효로인 학생수
+table(exam$address == "원효로")
+#exam에서 성별이 여성이 아닌 학생수
+table(exam$gender != "Female")
+
+#exam에서 1) 수학점수가 50점인 학생수
+table(exam$math == 50)
+#2) 50점이 아닌 학생수
+table(exam$math != 50)
+#3) 50점 이하인 학생수
+table(exam$math <= 50)
+#4) 50점 이상인 학생수
+table(exam$math >= 50)
+
+###논리 연산자
+#1) 영어점수가 50점 이하이고, 역사점수가 80점 이상인 학생은 몇 명인가?
+table(exam$english <= 50 & exam$history >= 80)
+#2) 수학점수가 90점 이상이거나, 역사점수가 90점 이상인 학생은 몇 명인가?
+table(exam$math >= 90 | exam$history >= 90)
+#3) 주소가 효창동이거나, 청파동이거나, 서계동인 학생은 몇 명인가?
+table(exam$address == "효창동" | exam$address == "청파동" | exam$address == "서계동")
+table(exam$address %in% c("효창동", "청파동", "서계동"))
+
+
+###변수명 바꾸기
+library(ggplot2)
+mpg <- mpg
+mpg
+str(mpg)
+summary(mpg)
+table(mpg$drv)
+table(mpg$fl)
+library(dplyr)
+mpg <- rename(mpg, fuel = fl)
+mpg <- rename(mpg, city = cty)
+mpg <- rename(mpg, highway = hwy)
+
+
+###측정값 바꾸기
+ifelse("A"=="B", "True", "False")
+v1 <- 3
+ifelse(v1 >= 3, "Y", "N")
+mpg$drv <- ifelse(mpg$drv == "f", "forward", mpg$drv)
+mpg$drv <- ifelse(mpg$drv == "r", "rear", mpg$drv)
+
+mpg$fuel <- ifelse(mpg$fuel == "r", "regular", mpg$fuel)
+mpg$fuel <- ifelse(mpg$fuel == "c", "CNG", mpg$fuel)
+mpg$fuel <- ifelse(mpg$fuel == "p", "premium", mpg$fuel)
+mpg$fuel <- ifelse(mpg$fuel == "e", "ethanol", mpg$fuel)
+mpg$fuel <- ifelse(mpg$fuel == "d", "diesel", mpg$fuel)
+
