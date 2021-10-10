ed83004d0044f6573aec357625f534a37f1447d4
윤정인 <yoonjeongin@naver.com>
2021-10-10 22:36:43 +0900

    data preprocessing: group_by, summarise

--- preprocessing.R
+++ preprocessing.R
@@ -75,3 +75,42 @@ exam <- exam %>% mutate(average = (math + english + history)/3)
 exam <- exam %>% mutate(total = math + english + history, average = (math + english + history)/3)
 
 exam$average <- round(exam$average, digits=2) #average값 소수점 자리수 조절
+exam %>% mutate(test=ifelse(total>=180, "pass", "fail"))
+
+### 11. 파생변수 sum(통합연비: 도심연비와 고속도로연비 합산)을 만드시오.
+mpg <- mpg %>% mutate(sum = city + highway)
+
+### 12. 통합연비의 평균은 얼마인가?
+mean(mpg$sum)
+
+### 13. 파생변수 avg(평균연비: 도심연비와 고속도로연비 평균)를 만들고, 평균 연비가 가장 높은 자동차 모델 세 개가 무엇인지 확인하시오.
+mpg <- mpg %>% mutate(avg = (city + highway)/2) 
+mpg %>% arrange(-avg) %>% head(3)
+
+
+
+### group_by 함수와 summarise 함수 ###
+
+#반 별로 학생수(빈도)와 수학점수의 평균, 표준편차 제시
+exam %>% group_by(class) %>% summarise(n(), mean(math), sd(math))
+exam %>% group_by(class) %>% summarise(count = n(), mean_math = mean(math), sd_math = sd(math))
+
+exam_clshist <- exam %>% group_by(class, gender) %>% summarise(count = n(), mean_history = mean(history))
+
+#세부 집단별 학생들의 구성비율을 보여주는 새로운 변수(perc) 만들기
+exam_clshist <- exam_clshist %>% mutate(perc = count/sum(count))
+exam_clshist <- exam_clshist %>% mutate(prop = count/sum(exam_clshist$count)) #prop는 전체 학생대비 비율 변수
+
+
+### 14. 자동차 모델을 제조사별, 구동방식별로 구분한 후, 도심연비평균과 고속도로연비평균 요약 결과를 제시하시오.
+mpg %>% group_by(manufacturer, drv) %>% summarise(mean(city), mean(highway)) %>% print(n=100)
+
+### 15. 자동차 모델을 제조사별로 구분한 후, suv 모델에 대해 통합 연비 평균 상위 3개 모델을 구하시오.
+mpg %>% group_by(manufacturer) %>% filter(class=="suv") %>% summarise(mean_sum = mean(sum)) %>% arrange(-mean_sum) %>% head(3)
+
+### 16. 평균 배기량이 가장 높은 세 개 변속기를 제시하시오
+#변수기별 구분 -> 평균 배기량 구하기 -> arrange -> head -> summarise
+mpg %>% group_by(trans) %>% summarise(mean_displ = mean(displ)) %>% arrange(-mean_displ) %>% head(3)
+
+### 17. 4기통 모델을 가장 많이 생산하는 업체 세 곳을 순서대로 구하시오.
+mpg %>% group_by(manufacturer) %>% filter(cyl == 4) %>% summarise(count = n()) %>% arrange(-count) %>% head(3)
