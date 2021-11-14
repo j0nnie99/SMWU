0452107e9ea90770261204fa0396d1a0d399f870
윤정인 <yoonjeongin@naver.com>
2021-11-14 23:52:43 +0900

    map graph 1

--- map.R
+++ map.R
@@ -1,3 +1,15 @@
 crime <- USArrests
 library(tibble)
 crime <- rownames_to_column(crime, var = "state")
+crime$state <- tolower(crime$state)
+
+install.packages("maps")
+library(maps)
+library(ggplot2)
+states_map <- map_data("state")
+
+install.packages("mapporj")
+library(mapproj)
+install.packages("ggiraphExtra")
+library(ggiraphExtra)
+ggChoropleth(data = crime, aes(fill=Murder, map_id=state), map = states_map, interactive = T)
