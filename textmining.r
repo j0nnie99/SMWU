59f2be220d128e482abdae0467a506fd16e17f43
윤정인 <yoonjeongin@naver.com>
2021-12-03 22:35:16 +0900

    text mining 03: word cloud

--- textmining.r
+++ textmining.r
@@ -28,6 +28,23 @@ library(extrafont)
 font_import()
 library(ggplot2)
 theme_set(theme_grey(base_family="AppleGothic"))
-###∂
+###
 
 ggplot(top20, aes(reorder(word, -n), n, fill = word)) + geom_bar(stat = "identity") + geom_text(aes(label = n ), hjust = -0.3) + labs(title = "문재인 출마 연설문 단어 빈도") + theme(title = element_text(size=12))
+
+Sys.setlocale("LC_ALL", "ko_KR.UTF-8")
+install.packages("ggwordcloud")
+library(ggwordcloud)
+ggplot(word_space, aes(label = word, size = n)) + geom_text_wordcloud(seed = 1234, family="AppleGothic") + scale_radius(limits = c(3,NA), range = c(3,10))
+
+#colors + background
+ggplot(word_space, aes(label = word, size = n, col = n)) + geom_text_wordcloud(seed = 1234, family="AppleGothic") + scale_radius(limits = c(3,NA), range = c(3,10)) + scale_color_gradient(low ="darkgreen", high="darkred") + theme_minimal()
+
+#font
+install.packages("showtext")
+library(showtext)
+font_add_google(name = "Nanum Gothic", family = "nanumgothic")
+showtext_auto()
+
+ggplot(top20, aes(reorder(word, -n), n, fill = word)) + geom_bar(stat = "identity") + geom_text(aes(label = n ), hjust = -0.3) + labs(title = "문재인 출마 연설문 단어 빈도") + theme(title = element_text(size=12), text = element_text(family = "nanumgothic"))
+ggplot(word_space, aes(label = word, size = n, col = n)) + geom_text_wordcloud(seed = 1234, family="nanumgothic") + scale_radius(limits = c(3,NA), range = c(3,10)) + scale_color_gradient(low ="darkgreen", high="darkred") + theme_minimal()
